d7e362b470994b6b0b59dd4c543c2a0d4a6167a9
William Hansen-Baird <william.hansen.baird@icloud.com>
2026-02-01 07:45:56 -0500

    Changed

--- SnipeUrType/Assets/Scripts/cutscene.cs
+++ SnipeUrType/Assets/Scripts/cutscene.cs
@@ -7,12 +7,25 @@ public class cutscene : MonoBehaviour
     public string nextSceneName;
 
     private VideoPlayer videoPlayer;
+    private bool loading = false;
 
     void Awake()
     {
         videoPlayer = GetComponent<VideoPlayer>();
         videoPlayer.loopPointReached += OnVideoFinished;
         videoPlayer.Play();
+    }
+
+    private void Update()
+    {
+        if (loading) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            loading = true;
+            videoPlayer.Stop();
+            SceneManager.LoadScene(nextSceneName);
+        }
     }
 
     void OnVideoFinished(VideoPlayer vp)
