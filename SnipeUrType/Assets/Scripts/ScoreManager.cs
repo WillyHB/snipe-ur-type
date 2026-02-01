ae5276c0481efe3b63ab0ef58bfd187a206ce804
amatdsotm <benjamin.carpiomak@gmail.com>
2026-02-01 11:17:29 -0500

    added review logic

--- SnipeUrType/Assets/Scripts/ScoreManager.cs
+++ SnipeUrType/Assets/Scripts/ScoreManager.cs
@@ -17,6 +17,7 @@ public class ScoreManager : MonoBehaviour
     public GameObject hearts;
     public GameObject brokenhearts;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject review;
     void Start()
     {
         Cursor.visible = true;
@@ -24,6 +25,7 @@ public class ScoreManager : MonoBehaviour
         Debug.Log($"Score scene cursor: visible={Cursor.visible}, lock ={Cursor.lockState}");
         status = ScoreSession.status;
         score = ScoreSession.score;
+        review.SetActive(false);
 
         switch(status)
         {
@@ -35,7 +37,7 @@ public class ScoreManager : MonoBehaviour
                 ShowText("Missed :(");
                 PlayFailure();
                 break;
-            default: //hit person
+            case 2: //hit person
                 ShowText("Score: " + score.ToString()+ "%");
                 if (score >= 50)
                 {
@@ -45,6 +47,23 @@ public class ScoreManager : MonoBehaviour
                 {
                     PlayFailure();
                 }
+                //get reviews
+                review.SetActive(true);
+                if (score < 10) writeReview("“I can’t believe you matched me with my cousin >:(. You should quit this game now");
+                else if (score >= 10 && score < 20) writeReview("Is Cupid blind?");
+                else if (score >= 20 && score < 30) writeReview("Terrible service. 1/5 stars");
+                else if (score >= 30 && score < 40) writeReview("Maybe don’t match me with a serial cheater"); 
+                else if (score >= 40 && score < 50) writeReview("Ugh, should have just used a dating app instead");
+                else if (score >= 50 && score < 60) writeReview("The date was just ok");
+                else if (score >= 60 && score < 70) writeReview("This might be better than those dating apps…");
+                else if (score >= 70 && score < 80) writeReview("I think I’m giving love a second chance!");
+                else if (score >= 80 && score < 90) writeReview("I think I’ve just found the love of my life!");
+                else writeReview("Omg, Cupid is invited to my wedding");
+
+                break;
+            case 3: //kaitlin kills you
+                ShowText("You died:(");
+                PlayFailure();
                 break;
         }
         
@@ -67,5 +86,10 @@ public class ScoreManager : MonoBehaviour
     void ShowText(string text)
     {
         display.text = text;
+    }
+
+    void writeReview(string reviewtext)
+    {
+        review.transform.GetChild(0).GetComponent<TMP_Text>().text = reviewtext;
     }
 }
