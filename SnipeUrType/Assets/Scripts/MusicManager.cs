95f6b7a991ced1dd7a44dd3d18707c8598759a60
saaddannaoui24 <saadeddine@dannaoui.com>
2026-02-01 11:56:52 -0500

    added seamless music transition from main menu to credits, and modified credits (font + VA)

---
+++ SnipeUrType/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+    
+    [SerializeField] private string sceneA = "MainMenu";
+    [SerializeField] private string sceneB = "Credits";
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != sceneA && scene.name != sceneB)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
