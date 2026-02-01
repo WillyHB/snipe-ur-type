126d1cef5c348f0f94d245426ccbf7b02df175f2
William Hansen-Baird <william.hansen.baird@icloud.com>
2026-02-01 05:18:16 -0500

    Fixed Person + Nice Application

--- SnipeUrType/Assets/Scripts/Application.cs
+++ SnipeUrType/Assets/Scripts/Application.cs
@@ -19,9 +19,15 @@ public class Application : MonoBehaviour
     public GameObject Page;
 
     public TMP_FontAsset[] Fonts;
+
+    public bool canHide;
+    public GameObject[] DisableOnHide;
+
+    private bool pressed;
+    private bool hidden = true;
     void Start()
     {
-        LeanTween.moveLocalY(Page, 0, 1f).setEaseOutQuad();
+        if (!canHide) LeanTween.moveLocalY(Page, 0, 1f).setEaseOutQuad();
         TMP_FontAsset font = Helpers.GetRandom(Fonts);
 
         Name.font = font;
@@ -51,6 +57,32 @@ public class Application : MonoBehaviour
             string bottom = Helpers.GetRandom(applicant.IdealAttributes.BottomType.Descriptions);
             string beard = Helpers.GetRandom(applicant.IdealAttributes.ShoeType.Descriptions) ?? "Has no beard. None at all.";
             Physical.text = hair + "\n" + body + "\n" + shoe + "\n" + top + "\n" + bottom + "\n" + beard;
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!canHide) return;
+            if (LeanTween.isTweening(Page)) return;
+
+            if (!pressed)
+            {
+                Page.transform.localPosition = new Vector2(0, hidden ? -500 : 0);
+                LeanTween.moveLocalY(Page, hidden ? 0 : -500, 0.25f).setEaseInOutQuad();
+                hidden = !hidden;
+                foreach (var go in DisableOnHide)
+                {
+                    go.SetActive(hidden);
+                }
+            }
+            pressed = true;
+        }
+        else
+        {
+            pressed = false;
         }
     }
 }
